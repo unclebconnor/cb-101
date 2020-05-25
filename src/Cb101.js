317da1a8d15bbb909efb91674199d4857d10fcee
unclebconnor <brian.connor@datadoghq.com>
2020-05-25 11:27:41 -0700

    basic setup and imports

---
+++ src/Cb101.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Cb101
+() {
+  return (
+    <div className="columns">
+      <div className="column is-8 is-mobile">
+        <div>Main Section</div>
+      </div>
+    </div>
+  );
+}
+
+export default Cb101
+;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Cb101 from './Cb101';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Cb101 />
   </React.StrictMode>,
   document.getElementById('root')
 );
