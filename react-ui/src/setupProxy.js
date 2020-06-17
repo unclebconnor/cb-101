50d2927102a75e876c9623e84dab6e86ca0dd002
unclebconnor <brian.connor@datadoghq.com>
2020-06-16 21:58:59 -0700

    dynamic port

--- react-ui/src/setupProxy.js
+++ react-ui/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: `http://localhost:${process.env.PORT || 5000}`,
       changeOrigin: true,
     })
   );
